979ad409210efa8676de8968477b7f6a5d9c7707
pynicole <nicolerodgers99@gmail.com>
2022-07-27 19:17:09 -0500

    adding files

--- App.js
+++ App.js
@@ -1,12 +1,65 @@
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import './index.css';
+import Movie from './components/Movie';
+
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4321a11430ac89d5155aac3326a46654&page=1";
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=4321a11430ac89d5155aac3326a46654&query=";
+
+function App() {
+  const [ movies, setMovies ] = useState([]);
+  const [ searchTerm, setSearchTerm ] = useState('');
+
+  useEffect(() => {
+    getMovies(FEATURED_API);
+  }, []);
+
+  const getMovies = (API) => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((data) => {
+        //console.log(data);
+        setMovies(data.results);
+      });
+  }
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if(searchTerm) {
+      getMovies(SEARCH_API + searchTerm);
+      setSearchTerm('');
+    }
+  };
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <header>
+        <form onSubmit={handleOnSubmit}>
+          <input 
+            className="search"
+            type="search" 
+            placeHolder="Search..." 
+            value={searchTerm}
+            onChange={handleOnChange}
+          />
+        </form>  
+      </header>
+
+      <div className="movie-container">
+        {movies.length > 0 &&
+        movies.map((movie) => (
+          <Movie key={movie.id} {...movie}/>
+        ))}
+      </div>
+    </>
   );
 }
 
@@ -18,3 +71,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
